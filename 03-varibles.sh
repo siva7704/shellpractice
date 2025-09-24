c6795a1d10fe31dd74f7da69fe779eb7ca779450
siva7704 <sivaramakrishna7703@gmail.com>
2025-09-24 11:03:22 +0530

    variables

---
+++ 03-varibles.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#No space before or after equal
+person1=Rahul
+person2=sachin
+
+echo "$person1:: Hi $person2, How are you"
+echo "$person2:: Hello $person1 I am fine, How are you"
+echo "$person1:: What are you learning recently"
+echo "$person2:: I am learning shell scrpting $person1"
